01b819246fbdc957e13f6d1a575c591034adda42
Sebastian Nemak <s7nio@users.noreply.github.com>
2017-07-31 13:56:05 +0200

    initial project commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Spring Boot Showcase
+
+## spring boot init
+
+https://start.spring.io
+
+## spring boot actuator
+
+## spring boot security
---
+++ src/main/java/de/codecentric/springbootshowcase/SpringBootShowcaseApplication.java
@@ -0,0 +1,12 @@
+package de.codecentric.springbootshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootShowcaseApplication.class, args);
+	}
+}
