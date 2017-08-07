dab36c3e46ae49a0bd57cf7030e0a1c8fd54e906
Sebastian Nemak <s7nio@users.noreply.github.com>
2017-08-07 23:34:45 +0200

    Add gauge & counter service and insecure endpoints

--- src/main/java/de/codecentric/springbootshowcase/controller/EchoController.java
+++ src/main/java/de/codecentric/springbootshowcase/controller/EchoController.java
@@ -2,6 +2,9 @@ package de.codecentric.springbootshowcase.controller;
 
 import de.codecentric.springbootshowcase.domain.EchoMessage;
 import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +15,12 @@ public class EchoController {
 
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    private GaugeService gaugeService;
+
+    @Autowired
+    private CounterService counterService;
+
     @RequestMapping("/echo")
     public String echo(@RequestParam(value = "text", defaultValue = "Hello World") final String text) {
         return String.format("Message %d: %s%n", counter.incrementAndGet(), text);
@@ -19,6 +28,8 @@ public class EchoController {
 
     @RequestMapping(value = "/echo/json", method = RequestMethod.GET, produces = "application/json")
     public EchoMessage echoJson(@RequestParam(value = "text", defaultValue = "Hello World") final String text) {
+        this.counterService.increment("myCounter");
+        this.gaugeService.submit("myRequestTime", 4.4d);
         return new EchoMessage(text);
     }
 }
