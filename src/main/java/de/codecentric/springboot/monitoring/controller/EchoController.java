98babac905f0163ebdacd2659f373ec7f817fe44
Sebastian Nemak <s7nio@users.noreply.github.com>
2017-08-22 00:02:03 +0200

    Add exception controller
    * add random exception controller
    * move gauge and counter into random exception controller

--- src/main/java/de/codecentric/springboot/monitoring/controller/EchoController.java
+++ src/main/java/de/codecentric/springboot/monitoring/controller/EchoController.java
@@ -1,11 +1,7 @@
 package de.codecentric.springboot.monitoring.controller;
 
 import de.codecentric.springboot.monitoring.domain.EchoMessage;
-import java.util.Random;
 import java.util.concurrent.atomic.AtomicLong;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.actuate.metrics.CounterService;
-import org.springframework.boot.actuate.metrics.GaugeService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,30 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EchoController {
 
-    private final AtomicLong atomicCounter;
-    private final GaugeService gaugeService;
-    private final CounterService counterService;
-
-    @Autowired
-    public EchoController(final GaugeService gaugeService, final CounterService counterService) {
-        this.gaugeService = gaugeService;
-        this.counterService = counterService;
-        this.atomicCounter = new AtomicLong();
-    }
+    private final AtomicLong atomicCounter = new AtomicLong();
 
     @RequestMapping(value = "/echo")
-    public String echo(@RequestParam(value = "text", defaultValue = "Hello World") final String text) {
+    public String echo(@RequestParam(value = "text") final String text) {
         return getMessage(text).getMessage();
     }
 
     @RequestMapping(value = "/echo/json", method = RequestMethod.GET, produces = "application/json")
     public EchoMessage echoJson(@RequestParam(value = "text", defaultValue = "Hello World") final String text) {
-        this.counterService.increment("myCounter");
-        this.gaugeService.submit("myRequestTime", new Random().nextDouble());
         return getMessage(text);
     }
 
     private EchoMessage getMessage(final String text) {
-        return new EchoMessage(String.format("%d: %s", atomicCounter.incrementAndGet(), text));
+        return new EchoMessage(
+                String.format("%d: %s", atomicCounter.incrementAndGet(), text));
     }
 }
---
+++ src/main/java/de/codecentric/springboot/monitoring/controller/RandomExceptionController.java
@@ -0,0 +1,32 @@
+package de.codecentric.springboot.monitoring.controller;
+
+import java.util.Random;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RandomExceptionController {
+
+    private final GaugeService gaugeService;
+    private final CounterService counterService;
+
+    @Autowired
+    public RandomExceptionController(final GaugeService gaugeService, final CounterService counterService) {
+        this.gaugeService = gaugeService;
+        this.counterService = counterService;
+    }
+
+    @RequestMapping(value = "/random/exception")
+    public String randomException() {
+        final double randomValue = new Random().nextDouble();
+        this.gaugeService.submit("myRequestRandomValue", randomValue);
+        if (randomValue > 0.5) {
+            this.counterService.increment("myExceptionCounter");
+            throw new IllegalStateException("random exception");
+        }
+        return "work well";
+    }
+}
